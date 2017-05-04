c9407f76a27b93a0db40baf7b8c74810d2c695f4
apurav2007 <apurav2007@gmail.com>
2017-05-04 23:45:26 +0530

    add project

---
+++ App/Containers/LaunchScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ScrollView, Text, Image, View } from 'react-native'
+import { Images } from '../Themes'
+
+// Styles
+import styles from './Styles/LaunchScreenStyles'
+
+export default class LaunchScreen extends React.Component {
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
+        <ScrollView style={styles.container}>
+          <View style={styles.centered}>
+            <Image source={Images.launch} style={styles.logo} />
+          </View>
+
+          <View style={styles.section} >
+            <Text style={styles.sectionText}>
+              {"It's time to start living the life we've imagined."}
+            </Text>
+          </View>
+
+        </ScrollView>
+      </View>
+    )
+  }
+}
