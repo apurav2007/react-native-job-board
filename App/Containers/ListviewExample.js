31dd99d485ee23c849afb92fdf14312cdb251980
apurav2007 <apurav2007@gmail.com>
2017-05-13 02:16:40 +0530

    load data from github job api

--- App/Containers/ListviewExample.js
+++ App/Containers/ListviewExample.js
@@ -17,68 +17,14 @@ class ListviewExample extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-             dataObjects: {}
+             joblist: {},
+             loaded : false
          }
-    /* ***********************************************************
-    * STEP 1
-    * This is an array of objects with the properties you desire
-    * Usually this should come from Redux mapStateToProps
-    *************************************************************/
-    
-    /*const dataObjects = [
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
-      {id:'1', title: 'Site Reliability Engineer (SRE) 1', location: 'Remote/Anywhere',type:'Full Time',company:'CircleCI',created :'24 May 2017', logo: 'http://github-jobs.s3.amazonaws.com/3f106ece-3113-11e7-81a3-7a4e0d644d3b.png'},
 
-
-    ]*/
-
-    /* ***********************************************************
-    * STEP 2
-    * Teach datasource how to detect if rows are different
-    * Make this function fast!  Perhaps something like:
-    *   (r1, r2) => r1.id !== r2.id}
-    *************************************************************/
-    const rowHasChanged = (r1, r2) => r1 !== r2
-
-    // DataSource configured
-    const ds = new ListView.DataSource({rowHasChanged})
-
-    // Datasource is always in state
-    this.state = {
-      dataSource: ds.cloneWithRows(dataObjects)
-    }
   }
 
 
-  componentWillMount () {
+componentWillMount () {
   this.loadjobs()
 }
 loadjobs() {
@@ -86,75 +32,55 @@ loadjobs() {
   api.getjob()
   .then((response) => {
     console.log(response.data)
-     this.setState({dataObjects : response.data})
-       console.log('Health feed Response : ', this.state.dataObjects)
+     this.setState({joblist : response.data, loaded : true})
+       console.log('Health feed Response : ', this.state.joblist)
   })
 
 }
 
-  /* ***********************************************************
-  * STEP 3
-  * `renderRow` function -How each cell/row should be rendered
-  * It's our best practice to place a single component here:
-  *
-  * e.g.
-    return <MyCustomCell title={rowData.title} description={rowData.description} />
-  *************************************************************/
-renderRow (rowData) {
-    return (
-      <TouchableOpacity onPress={() => NavigationActions.jobdetail()} >
-      <View style={styles.row}>
-        <Image source={Images.logo} style ={styles.image} />
-        <View style={styles.row2} >
-          <Text style={styles.boldLabelTitle}>{rowData.title}</Text>
-          <Text style={styles.boldLabel}>{rowData.location}</Text>
-          <Text style={styles.label}>{rowData.company}</Text>
-          <Text style={styles.label}>{rowData.type}</Text>
-          <Text style={styles.label}>{rowData.created}</Text>
-        </View>
 
-      </View>
-      </TouchableOpacity>
+render () {
+    return (
+      <Content>
+      {  this.state.loaded ? this.state.joblist.map(item => (
+                  <Jobs key={item.id.toString()} job={item} />
+                )) : null }
+      </Content>
     )
   }
+}
 
-  /* ***********************************************************
-  * STEP 4
-  * If your datasource is driven by Redux, you'll need to
-  * reset it when new data arrives.
-  * DO NOT! place `cloneWithRows` inside of render, since render
-  * is called very often, and should remain fast!  Just replace
-  * state's datasource on newProps.
-  *
-  * e.g.
-    componentWillReceiveProps (newProps) {
-      if (newProps.someData) {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(newProps.someData)
-        })
-      }
+class Jobs extends React.Component {
+
+  constructor(props) {
+        super(props);
+        // this.state = {
+        //
+        // }
     }
-  *************************************************************/
+    componentDidMount () {
+      //this.commentsCounts()
+    }
 
-  // Used for friendly AlertMessage
-  // returns true if the dataSource is empty
-  noRowData () {
-    return this.state.dataSource.getRowCount() === 0
-  }
 
-  render () {
-    return (
-      <View style={styles.container}>
-        <AlertMessage title='Nothing to See Here, Move Along' show={this.noRowData()} />
-        <ListView
-          contentContainerStyle={styles.listContent}
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow}
-          pageSize={15}
-        />
-      </View>
-    )
-  }
+
+ render () {
+return (
+  <TouchableOpacity onPress={() => NavigationActions.jobdetail()} >
+  <View style={styles.row} >
+    <Image source={Images.logo} style ={styles.image} />
+    <View style={styles.row2} >
+      <Text style={styles.boldLabelTitle}>{this.props.job.title}</Text>
+      <Text style={styles.boldLabel}>{this.props.job.location}</Text>
+      <Text style={styles.label}>{this.props.job.company}</Text>
+      <Text style={styles.label}>{this.props.job.type}</Text>
+      <Text style={styles.label}>{this.props.job.created_at}</Text>
+    </View>
+
+  </View>
+  </TouchableOpacity>
+)
+ }
 }
 
 const mapStateToProps = (state) => {
